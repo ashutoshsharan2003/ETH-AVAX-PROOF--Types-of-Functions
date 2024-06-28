44a7fb4180123b43c0ff6ea35dd211846780e687
ashutoshsharan2003 <141537249+ashutoshsharan2003@users.noreply.github.com>
2024-06-28 22:23:17 +0530

    Init

---
+++ Contract/WakandaPower.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WakandaPower is ERC20, Ownable {
+    uint256 public TOTAL_SUPPLY;
+
+    constructor(address _owner) ERC20("WakandaPower", "WKN") Ownable(_owner) {
+        TOTAL_SUPPLY = 100000000 * 10**uint256(decimals());
+        _mint(_owner, TOTAL_SUPPLY);
+    }
+function transferWithUsage(address recipient, uint256 amount /*,uint256 usage*/) public {
+    
+
+        transfer(recipient, amount);
+        // Additional logic to record energy usage can be added here
+    }
+
+    function airdrop(address[] calldata recipients, uint256[] calldata amounts) public onlyOwner {
+        require(recipients.length == amounts.length, "Unequal recipients and amounts");
+        for (uint256 i = 0; i < recipients.length; i++) {
+            transfer(recipients[i], amounts[i]);
+        }
+    }
+}
